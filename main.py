d9e0e1f3ca80c58fb59d1a71ed76d963eb664cef
Ambareesha AV <ambareesha7@users.noreply.github.com>
2023-10-28 23:15:30 +0530

    added dropdown to select audio or video

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 import flet as ft
 
-
-# def main(page: ft.Page):
-#     page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
-
-
-# ft.app(main)
 from pytube import YouTube
 import os
+import pathlib as pt
 import flet as ft
 from flet import Text, TextField, ElevatedButton, Row, Column, Image
 from flet_core.control_event import ControlEvent
 
 # replace destination with the path where you want to save the downloaded file
 destination = "downloads/"
+video: str = 'video'
+audio: str = 'audio'
+both: str = 'both'
+# pt.Path.joinpath(os.getcwd(), )
 
 
 def main(page: ft.Page):
@@ -25,8 +24,23 @@ def main(page: ft.Page):
     link_txt_box: TextField = TextField(
         hint_text='click here to enter youtube link')
 
+    def on_dropdown_change(e):
+        print(f'dropdown change {dropdown_btn.value}')
+        page.update()
+
+    dropdown_btn: ft.Dropdown = ft.Dropdown(
+        on_change=on_dropdown_change,
+        width=120,
+        label='Video download type',
+        value=both,
+        options=[
+            ft.dropdown.Option(both),
+            ft.dropdown.Option(video),
+            ft.dropdown.Option(audio),
+        ],
+    )
     download_btn: ElevatedButton = ElevatedButton(
-        text="Download")
+        text="Download", disabled=True)
     image: Image = ft.Image(src="pic.png", width=400, height=350)
     make_dir(destination)
 
@@ -44,7 +58,7 @@ def main(page: ft.Page):
             if link_txt_box.value:
                 video_details = YouTube(
                     link_txt_box.value, on_progress_callback=on_progress)
-                title.value = video_details.title
+
                 show_snack_bar(
                     e=e, text=f'started downloading...\n{str(video_details.title)}')
                 progress_ring = ft.ProgressRing()
@@ -53,15 +67,30 @@ def main(page: ft.Page):
                 link_txt_box.disabled = True
                 download_btn.disabled = True
                 page.update()
-                video_details.streams.get_highest_resolution().download(destination)
+                format = str(dropdown_btn.value)
+                if (format == both):
+                    video_details.streams.get_highest_resolution().download(destination)
+                elif (format == video):
+                    dest = f'{destination}{video}/'
+                    make_dir(dest)
+                    video_details.streams.filter(
+                        only_video=True).first().download(dest)
+                elif (format == audio):
+                    dest = f'{destination}{audio}/'
+                    make_dir(dest)
+                    video_details.streams.get_audio_only().download(dest)
+                else:
+                    video_details.streams.get_highest_resolution().download(destination)
+
                 print(f"video_details.title {video_details.title}")
                 link_txt_box.disabled = False
-                download_btn.disabled = False
+                # download_btn.disabled = False
+                image.visible = False
                 page.update()
                 link_txt_box.value = ''
                 progress_ring.visible = False
                 show_snack_bar(
-                    e=e, text=f'{str(video_details.title)} download completed')
+                    e=e, text=f'download completed')
                 title.value = ''
                 page.update()
         except:
@@ -73,8 +102,14 @@ def main(page: ft.Page):
         try:
             print(f"entered text --{e.data}--")
             video_details = YouTube(e.data)
+            cc = video_details.streams.get_audio_only()
+            # cc = video_details.streams.()
+            print(f'audio..... {cc}')
             title.value = str(video_details.title)
             image.src = video_details.thumbnail_url
+            image.visible = True
+            download_btn.disabled = False
+            dropdown_btn.disabled = False
             print(f"video_details.title {video_details.title}, ")
             page.update()
 
@@ -86,16 +121,18 @@ def main(page: ft.Page):
     def add_widgets(page: ft.Page):
         page.clean
         page.add(Row(controls=[image], alignment=ft.MainAxisAlignment.CENTER))
+        image.visible = False
         page.add(Row(
             controls=[Text(value=f"video title:"),],
             alignment=ft.MainAxisAlignment.CENTER))
         page.add(Row(
             controls=[title,],
             alignment=ft.MainAxisAlignment.CENTER))
-        page.add(Column(controls=[link_txt_box, download_btn],
+        page.add(Column(controls=[link_txt_box, Row(alignment='center', controls=[download_btn, dropdown_btn]), ],
                         horizontal_alignment="center"))
     link_txt_box.on_change = on_text_changed
     download_btn.on_click = download
+
     # add widgets
     add_widgets(page=page)
 
